38cd438fea92cc11d1f726d94afe7647c754b084
ManeeshProjectX <maneeshmondal56@gmail.com>
2024-06-26 16:26:54 +0530

    test1

--- js/image-puzzle.js
+++ js/image-puzzle.js
@@ -1,93 +1,119 @@
-﻿var timerFunction;
+﻿// image-puzzle.js
 
-var imagePuzzle = {
-    stepCount: 0,
-    startTime: new Date().getTime(),
-    startGame: function (images, gridSize) {
-        this.setImage(images, gridSize);
-        $('#playPanel').show();
-        $('#sortable').randomize();
-        this.enableSwapping('#sortable li');
-        this.stepCount = 0;
-        this.startTime = new Date().getTime();
-        this.tick();
-    },
-    tick: function () {
-        var now = new Date().getTime();
-        var elapsedTime = parseInt((now - imagePuzzle.startTime) / 1000, 10);
-        $('#timerPanel').text(elapsedTime);
-        timerFunction = setTimeout(imagePuzzle.tick, 1000);
-    },
-    enableSwapping: function (elem) {
-        $(elem).draggable({
-            snap: '#droppable',
-            snapMode: 'outer',
-            revert: "invalid",
-            helper: "clone"
-        });
-        $(elem).droppable({
-            drop: function (event, ui) {
-                var $dragElem = $(ui.draggable).clone().replaceAll(this);
-                $(this).replaceAll(ui.draggable);
+var imagePuzzle = (function() {
+    var gridSize = 4;
+    var images = [];
+    var currentImage = {};
+    var timer;
+    var countdown;
+    var stepCount;
 
-                currentList = $('#sortable > li').map(function (i, el) { return $(el).attr('data-value'); });
-                if (isSorted(currentList))
-                    $('#actualImageBox').empty().html($('#gameOver').html());
-                else {
-                    var now = new Date().getTime();
-                    imagePuzzle.stepCount++;
-                    $('.stepCount').text(imagePuzzle.stepCount);
-                    $('.timeCount').text(parseInt((now - imagePuzzle.startTime) / 1000, 10));
-                }
+    function init(imgs) {
+        images = imgs;
+        gridSize = 4;
+        $('#imgTitle').text('');
+        $('#sortable').html('');
+        $('#actualImage').attr('src', '');
+        $('#stepBox .stepCount').text(0);
+        $('#timerPanel').text(90);
+        $('#levelPanel input:radio[value="4"]').prop('checked', true);
+        startGame(images, gridSize);
+    }
 
-                imagePuzzle.enableSwapping(this);
-                imagePuzzle.enableSwapping($dragElem);
+    function startGame(imgs, size) {
+        clearInterval(timer);
+        countdown = 90;
+        stepCount = 0;
+        $('#timerPanel').text(countdown);
+        $('#stepBox .stepCount').text(stepCount);
+        timer = setInterval(updateTimer, 1000);
+        currentImage = imgs[Math.floor(Math.random() * imgs.length)];
+        gridSize = size;
+        createGrid(currentImage, gridSize);
+        $('#actualImage').attr('src', currentImage.src);
+    }
+
+    function createGrid(image, size) {
+        var pieces = [];
+        for (var i = 0; i < size * size; i++) {
+            pieces.push('<li class="ui-state-default" data-pos="' + i + '" style="background-image:url(' + image.src + '); background-size: ' + size * 100 + '% ' + size * 100 + '%; background-position: ' + ((i % size) * 100 / (size - 1)) + '% ' + (Math.floor(i / size) * 100 / (size - 1)) + '%;"></li>');
+        }
+
+        pieces = shuffleArray(pieces);
+        $('#sortable').html(pieces.join(''));
+        $('#sortable').sortable({
+            stop: function(event, ui) {
+                stepCount++;
+                $('#stepBox .stepCount').text(stepCount);
+                checkOrder();
             }
         });
-    },
+        $('#sortable').disableSelection();
+    }
 
-    setImage: function (images, gridSize) {
-        console.log(gridSize);
-        gridSize = gridSize || 4; // If gridSize is null or not passed, default it as 4.
-        console.log(gridSize);
-        var percentage = 100 / (gridSize - 1);
-        var image = images[Math.floor(Math.random() * images.length)];
-        $('#imgTitle').html(image.title);
-        $('#actualImage').attr('src', image.src);
-        $('#sortable').empty();
-        for (var i = 0; i < gridSize * gridSize; i++) {
-            var xpos = (percentage * (i % gridSize)) + '%';
-            var ypos = (percentage * Math.floor(i / gridSize)) + '%';
-            var li = $('<li class="item" data-value="' + (i) + '"></li>').css({
-                'background-image': 'url(' + image.src + ')',
-                'background-size': (gridSize * 100) + '%',
-                'background-position': xpos + ' ' + ypos,
-                'width': 400 / gridSize,
-                'height': 400 / gridSize
-            });
-            $('#sortable').append(li);
+    function shuffleArray(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
         }
-        $('#sortable').randomize();
+        return array;
     }
-};
 
-function isSorted(arr) {
-    for (var i = 0; i < arr.length - 1; i++) {
-        if (arr[i] != i)
-            return false;
+    function checkOrder() {
+        var currentOrder = $('#sortable li').map(function() {
+            return $(this).data('pos');
+        }).get();
+
+        var isSolved = currentOrder.every(function(val, index) {
+            return val === index;
+        });
+
+        if (isSolved) {
+            clearInterval(timer);
+            $('#gameOver h2').text('You win!');
+            $('#gameOver').show();
+        }
     }
-    return true;
-}
-$.fn.randomize = function (selector) {
-    var $elems = selector ? $(this).find(selector) : $(this).children(),
-        $parents = $elems.parent();
 
-    $parents.each(function () {
-        $(this).children(selector).sort(function () {
-            return Math.round(Math.random()) - 0.5;
-        }).remove().appendTo(this);
+    function updateTimer() {
+        countdown--;
+        $('#timerPanel').text(countdown);
+        if (countdown <= 0) {
+            clearInterval(timer);
+            $('#gameOver').show();
+        }
+    }
+
+    return {
+        init: init,
+        startGame: startGame
+    };
+})();
+
+$(function() {
+    var images = [
+        { src: 'img/cat.jpeg' },
+        { src: 'img/cartoon.jpeg' },
+        { src: 'img/OIP.jpeg' },
+        { src: 'img/download.jpeg' },
+        { src: 'img/french_polynesia.jpeg' },
+        { src: 'img/south_africa.jpeg' },
+        { src: 'img/vietnam.jpeg' },
+        { src: 'img/scotland.jpeg' },
+        { src: 'img/scenery.jpeg' }
+    ];
+
+    imagePuzzle.init(images);
+
+    $('#newPhoto').click(function() {
+        var gridSize = $('#levelPanel :radio:checked').val();
+        imagePuzzle.startGame(images, gridSize);
     });
-    return this;
-};
 
-
+    $('#levelPanel :radio').change(function(e) {
+        var gridSize = $(this).val();
+        imagePuzzle.startGame(images, gridSize);
+    });
+});
